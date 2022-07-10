67d1822b804e8188073380d66466f20170a636d2
KimgiaiiWuyi <444835641@qq.com>
2022-07-10 22:31:35 +0800

    refactor: `genshinuid_eventlist`

---
+++ genshinuid/util/genshin_fonts.py
@@ -0,0 +1,12 @@
+from PIL import ImageFont
+from pathlib import Path
+
+FONT_PATH = Path(__file__).parents[1] / 'resource' / 'yuanshen.ttf'
+FONT_ORIGIN_PATH = Path(__file__).parents[1] / 'resource' / 'yuanshen_origin.ttf'
+
+def genshin_font(size: int):
+    return ImageFont.truetype(str(FONT_PATH), size=size)
+
+
+def genshin_font_origin(size: int) -> ImageFont:
+    return ImageFont.truetype(str(FONT_ORIGIN_PATH), size=size)
