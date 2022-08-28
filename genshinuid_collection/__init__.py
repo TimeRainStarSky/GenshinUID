6c7ec1a64c9eb9e3a371cd2a2091bb55248162f8
KimgiaiiWuyi <444835641@qq.com>
2022-08-28 20:19:22 +0800

    ✨ `genshinuid_collection`

--- genshinuid_collection/__init__.py
+++ genshinuid_collection/__init__.py
@@ -5,30 +5,26 @@ from ..utils.message.get_image_and_at import ImageAndAt
 from ..utils.message.error_reply import *  # noqa: F403,F401
 from ..utils.mhy_api.convert_mysid_to_uid import convert_mysid
 
-get_collection_info = on_regex(
+
+@sv.on_rex(
     r'^(\[CQ:at,qq=[0-9]+\])?( )?'
     r'(uid|查询|mys)?([0-9]+)?'
     r'(收集|宝箱|sj|bx)'
     r'(\[CQ:at,qq=[0-9]+\])?( )?$',
-    block=True,
 )
-
-
-@get_collection_info.handle()
-@handle_exception('查询收集信息')
-async def send_collection_info(
-    event: Union[GroupMessageEvent, PrivateMessageEvent],
-    matcher: Matcher,
-    args: Tuple[Any, ...] = RegexGroup(),
-    custom: ImageAndAt = Depends(),
-):
+async def send_collection_info(bot: HoshinoBot, ev: CQEvent):
+    args = ev['match'].groups()
     logger.info('开始执行[查询收集信息]')
     logger.info('[查询收集信息]参数: {}'.format(args))
-    at = custom.get_first_at()
+    at = re.search(r'\[CQ:at,qq=(\d*)]', str(ev.message))
+
     if at:
-        qid = at
+        qid = int(at.group(1))
     else:
-        qid = event.user_id
+        if ev.sender:
+            qid = int(ev.sender['user_id'])
+        else:
+            return
 
     if args[2] != 'mys':
         if args[3] is None:
@@ -43,12 +39,13 @@ async def send_collection_info(
     logger.info('[查询收集信息]uid: {}'.format(uid))
 
     if '未找到绑定的UID' in uid:
-        await matcher.finish(UID_HINT)
+        await bot.send(ev, UID_HINT)
 
     im = await draw_collection_img(uid)
     if isinstance(im, str):
-        await matcher.finish(im)
+        await bot.send(ev, im)
     elif isinstance(im, bytes):
-        await matcher.finish(MessageSegment.image(im))
+        im = await convert_img(im)
+        await bot.send(ev, im)
     else:
-        await matcher.finish('发生了未知错误,请联系管理员检查后台输出!')
+        await bot.send(ev, '发生了未知错误,请联系管理员检查后台输出!')
