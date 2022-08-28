cda7575a87ad40053b234297f7f78182765665bc
KimgiaiiWuyi <444835641@qq.com>
2022-08-28 20:48:00 +0800

    ✨ `genshinuid_gachalogs`

--- genshinuid_gachalog/__init__.py
+++ genshinuid_gachalog/__init__.py
@@ -6,46 +6,37 @@ from ..utils.message.get_image_and_at import ImageAndAt
 from ..utils.message.error_reply import *  # noqa: F403,F401
 from ..utils.mhy_api.get_mhy_data import get_gacha_log_by_authkey
 
-get_gacha_log = on_command('刷新抽卡记录')
-get_gacha_log_card = on_command('抽卡记录')
 
-
-@get_gacha_log_card.handle()
-@handle_exception('抽卡记录')
-async def send_gacha_log_card_info(
-    event: Union[GroupMessageEvent, PrivateMessageEvent],
-    matcher: Matcher,
-    custom: ImageAndAt = Depends(),
-    args: Tuple[Any, ...] = CommandArg(),
-):
+@sv.on_fullmatch('抽卡记录')
+async def send_gacha_log_card_info(bot: HoshinoBot, ev: CQEvent):
     logger.info('开始执行[抽卡记录]')
-    if args:
+    if ev.sender:
+        qid = int(ev.sender['user_id'])
+    else:
         return
-    uid = await select_db(event.user_id, mode='uid')
+    uid = await select_db(qid, mode='uid')
+
     if isinstance(uid, str):
         im = await draw_gachalogs_img(uid)
         if isinstance(im, bytes):
-            await matcher.finish(MessageSegment.image(im))
+            im = await convert_img(im)
+            await bot.send(ev, im)
         else:
-            await matcher.finish(im)
+            await bot.send(ev, im)
     else:
-        await matcher.finish(UID_HINT)
+        await bot.send(ev, UID_HINT)
 
 
-@get_gacha_log.handle()
-@handle_exception('刷新抽卡记录')
-async def send_daily_info(
-    event: Union[GroupMessageEvent, PrivateMessageEvent],
-    matcher: Matcher,
-    custom: ImageAndAt = Depends(),
-    args: Tuple[Any, ...] = CommandArg(),
-):
+@sv.on_fullmatch('刷新抽卡记录')
+async def send_daily_info(bot: HoshinoBot, ev: CQEvent):
     logger.info('开始执行[刷新抽卡记录]')
-    if args:
+    if ev.sender:
+        qid = int(ev.sender['user_id'])
+    else:
         return
-    uid = await select_db(event.user_id, mode='uid')
+    uid = await select_db(qid, mode='uid')
     if isinstance(uid, str):
         im = await save_gachalogs(uid)
-        await matcher.finish(im)
+        await bot.send(ev, im)
     else:
-        await matcher.finish(UID_HINT)
+        await bot.send(ev, UID_HINT)
